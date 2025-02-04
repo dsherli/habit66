29b60e5673fdeddbd53ae3c78b30c42626625ac7
Dillon <dillon.sherling@wsu.edu>
2025-02-03 17:32:14 -0800

    initial commit

---
+++ src/main/java/com/dsherli/habit66/Habit66Application.java
@@ -0,0 +1,13 @@
+package com.dsherli.habit66;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Habit66Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Habit66Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsherli/habit66/Habit66ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsherli.habit66;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Habit66ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
