93bd609a908778a0d39f11ea32f10acb66c7ea49
Dillon <dillon.sherling@wsu.edu>
2025-02-10 19:23:18 -0800

    complete commit, added HabitRepository and UserRepository classes (forgot to add in last commit)

--- backend/src/main/java/com/dsherli/habit66/repository/UserRepository.java
+++ backend/src/main/java/com/dsherli/habit66/repository/UserRepository.java
@@ -4,9 +4,17 @@ import com.dsherli.habit66.model.HabitUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<HabitUser, Long> {
-    HabitUser findByUsername(String username);
-    HabitUser findByEmail(String email);
-    
+
+    // Optional is a container object which may or may not contain a non-null value, this one is used to find a user by username
+    Optional<HabitUser> findByUsername(String username);
+
+    // Find a user by email
+    Optional<HabitUser> findByEmail(String email);
+
+    // Find a user by username or email (used for login)
+    Optional<HabitUser> findByUsernameOrEmail(String username, String email);
 }
